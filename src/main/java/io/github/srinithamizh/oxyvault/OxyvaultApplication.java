328592a631eb3f6760e8ee3c428db4db2ce71d99
srinithamizh <srinithamizh@gmail.com>
2026-07-23 13:35:53 +0530

    chore: oxyvault project setup

---
+++ src/main/java/io/github/srinithamizh/oxyvault/OxyvaultApplication.java
@@ -0,0 +1,13 @@
+package io.github.srinithamizh.oxyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxyvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxyvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/srinithamizh/oxyvault/OxyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.srinithamizh.oxyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
